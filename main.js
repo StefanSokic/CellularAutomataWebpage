2cf37a66b17eb9c13b6cd7ded0408bdd917779bf
Stefan Sokic <stefansokic@Stefans-MacBook-Pro.local>
2017-01-16 13:56:14 -0500

    changing alg to use a 2d array instead

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 
-for (var i = 0; i < 5500; i++) {
+//using rule 73
+
+for (var i = 0; i < 81; i++) {
 	var square = document.createElement("div");
 	if (binaryRand() == 0) {
-		square.className = "square";
+		square.className = "square-inactive";
 	} else {
 		square.className = "square-active";
 	}
@@ -12,3 +14,32 @@ for (var i = 0; i < 5500; i++) {
 function binaryRand() {
 	return Math.round(Math.random());
 }
+
+// 2d array approach
+
+/*  - create a 80x200 2d array
+	- the first row is randomly generated
+	- all following rows take shape based on the rows above them
+	- use the .map() function to append it all to the body
+*/
+
+var array = [];
+for (var row = 0; row < 200; row++) {
+	var innerArray = [];
+	for (var col = 0; col < 80; col++) {
+		// the first row get's filled with random binary digits
+		if (row == 0) {
+			innerArray.push(binaryRand());
+		} else { // the rest are filled based on the state of the row above it
+			if (array[row-1][col] == 1) {
+				innerArray.push("cell above is a 1");
+			} else {
+				innerArray.push("cell above is a 0");
+			}
+		}
+	}
+	array.push(innerArray);
+}
+
+console.log(array);
+
